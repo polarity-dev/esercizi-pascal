59290605d90b84fd3525966974d2a8b9db438a2b
Riccardo Rispoli <riccardo.rispoli@soluzionifutura.it>
2022-09-20 10:52:40 +0200

    Add array manipulation challenge

---
+++ challenges/array-manipulation/modulo.js
@@ -0,0 +1,32 @@
+module.exports.correctNegatives = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
+module.exports.lengths = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
+module.exports.removeOdds = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
+module.exports.goodWords = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
+module.exports.classify = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
+module.exports.sumOfSquares = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
+module.exports.aggressiveText = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
+module.exports.averageGrade = (arr) => {
+  throw new Error("NOT IMPLEMENTED")
+}
+
---
+++ challenges/array-manipulation/test.js
@@ -0,0 +1,16 @@
+const modulo = require("./modulo.js")
+
+const sampleNumberArray = [-2, -1, 0, 1, 2, 10, 20]
+const sampleStringArray = ["ciao", "mareee", "!", ""]
+/*
+
+console.log(modulo.correctNegatives(sampleNumberArray)) // [0, 0, 0, 1, 2, 10, 20]
+console.log(modulo.lengths(sampleStringArray)) // [4, 6, 1, 0]
+console.log(modulo.removeOdds(sampleNumberArray)) // [-2, 0, 2, 10, 20]
+console.log(modulo.goodWords(sampleStringArray), 1, 4) // ["ciao", "!"]
+console.log(modulo.classify(sampleNumberArray)) // ["acido", "acido", "basico"]
+console.log(modulo.sumOfSquares(sampleNumberArray)) // 4+1+0+1+4+100+400 = 510
+console.log(modulo.aggressiveText(sampleStringArray)) // "ciao. mareee. !."
+console.log(modulo.averageGrade(sampleNumberArray)) // (0+1+2+10)/4 = 3.25
+
+*/
